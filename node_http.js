88ca04b269cf7bf4152b52c3237a9762cfec07c3
psychO <98010443+sike-0@users.noreply.github.com>
2023-01-04 16:33:22 +0530

    Add files via upload

---
+++ node_http.js
@@ -0,0 +1,6 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.write("\u001b[40m A \u001b[41m B \u001b[42m C \u001b[43m D \u001b[0m");
+    res.end();
+}).listen(8080);
