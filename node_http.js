4a49d54967688d31a0f94a8eed197bb41ac804c0
psychO <98010443+sike-0@users.noreply.github.com>
2023-01-04 16:42:14 +0530

    Update node_http.js

--- node_http.js
+++ node_http.js
@@ -3,4 +3,4 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.write("\u001b[40m A \u001b[41m B \u001b[42m C \u001b[43m D \u001b[0m");
     res.end();
-}).listen(8080);
+}).listen(process.env.PORT);
